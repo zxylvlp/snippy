29bd86a9a6d15033e89573f3bf2d0fa86f128dfe
赵星宇 <zhaoxingyu19920404@gmail.com>
2017-06-02 11:47:07 +0800

    Create rsync_project.sh

---
+++ rsync_project.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+LOCAL_DIR=/local_dir/
+REMOTE_DIR=username@0.0.0.0:/remote_dir/
+DIRNAME=project_dir_name
+fswatch -r $LOCAL_DIR$DIRNAME | while read line; do
+    if [[ $line == *'.git/index.lock' ]]; then
+        continue
+    fi
+    if [ ! -e "$line" ]; then
+        rsync -avz --exclude *'.git/index.lock' $LOCAL_DIR$DIRNAME $REMOTE_DIR
+    else
+        rsync -avz ${line##*$LOCAL_DIR} ${REMOTE_DIR}${line##*$FROMDIR}
+    fi
+done
